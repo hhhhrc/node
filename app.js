55aaf7bed660dc00bfb826473a449d50694fa50a
huricha <huricha@intra.nsfocus.com>
2020-01-13 19:31:19 +0800

    接口写完

--- app.js
+++ app.js
@@ -2,9 +2,36 @@ const queryString = require('querystring')
 const handleBlogRouter = require('./src/router/blog');
 const handleUserRouter = require('./src/router/user');
 
+const getPostData = (req) => {
+    const promise = new Promise((resolve, reject) => {
+        if (req.method !== 'POST') {
+            resolve({})
+            return
+        }
+        if (req.headers['content-type'] !== 'application/json') {
+            resolve({})
+            return
+        }
+        let postData = ''
+        req.on('data', chunk => {
+            postData += chunk.toString
+        })
+        req.on('end', () => {
+            if (!postData) {
+                resolve({})
+                return
+            }
+            resolve(
+                JSON.parse(postData)
+            )
+        })
+    })
+    return promise
+}
+
 const serverHandle = (req, res) => {
     // 设置返回格式
-    res.setHeader('Content-Type', 'application/json')
+    res.setHeader('Content-type', 'application/json')
 
     //获取path
     const url = req.url;
@@ -13,28 +40,32 @@ const serverHandle = (req, res) => {
     //解析query
     req.query = queryString.parse(url.split('?')[1])
 
-    //处理blog
-    const blogData = handleBlogRouter(req, res)
-    if (blogData) {
-        res.end(
-            JSON.stringify(blogData)
-        )
-        return
-    }
+    getPostData(req).then(postData => {
+        req.body = postData
+        //处理blog
+        const blogData = handleBlogRouter(req, res)
+        if (blogData) {
+            res.end(
+                JSON.stringify(blogData)
+            )
+            return
+        }
 
-    //处理user
-    const userData = handleUserRouter(req, res)
-    if (userData) {
-        res.end(
-            JSON.stringify(userData)
-        )
-        return
-    }
+        //处理user
+        const userData = handleUserRouter(req, res)
+        if (userData) {
+            res.end(
+                JSON.stringify(userData)
+            )
+            return
+        }
 
-    // 未命中，404,纯文本
-    res.writeHead(404, { 'content-type': 'text/plain' });
-    res.write('404 not found');
-    res.end()
+        // 未命中，404,纯文本
+        res.writeHead(404, { 'content-type': 'text/plain' });
+        res.write('404 not found');
+        res.end()
+    })
+
 }
 
 module.exports = serverHandle
